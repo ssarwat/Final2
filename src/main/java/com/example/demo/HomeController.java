ecc6d442ba406a393606049d83f2e8014f74b439
ssarwat <sumaiyah_sarwat@yahoo.com>
2019-08-22 15:32:54 -0400

    first commit

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,43 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.jws.WebParam;
+import javax.validation.Valid;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    MessagesRepository messagesRepository;
+
+    //For the home page path pointing to a list of all MESSAGES
+    @RequestMapping("/")
+    public String listMessages(Model model){
+        model.addAttribute("messages", messagesRepository.findAll());
+        return "list";
+    }
+
+    //For the add pat for adding messages
+    @GetMapping("/add")
+    public String addMessages(Model model){
+        model.addAttribute("message", new Messages());
+        return "messagepage";
+    }
+
+    @PostMapping("/process")
+    public String processMessages(@Valid Messages message, BindingResult result){
+        if(result.hasErrors()){
+            return "messagepage";
+        }
+        messagesRepository.save(message);
+        return "redirect:/";
+    }
+
+}
---
+++ src/main/java/com/example/demo/Messages.java
@@ -0,0 +1,58 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Messages {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotNull
+    @Size(min = 1, max = 150)
+    private String content;
+
+    @NotNull
+    private String postDate;
+
+    @NotNull
+    private String sentby;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getPostDate() {
+        return postDate;
+    }
+
+    public void setPostDate(String postDate) {
+        this.postDate = postDate;
+    }
+
+    public String getSentby() {
+        return sentby;
+    }
+
+    public void setSentby(String sentby) {
+        this.sentby = sentby;
+    }
+}
